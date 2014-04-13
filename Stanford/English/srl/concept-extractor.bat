0ae49d81d0ce32df3314bd2ee372e6a1a4fea171
shafqatvirk <virk.shafqat@gmail.com>
2014-04-13 11:51:16 +0800

    modifiction in concept-extractor

--- Stanford/English/srl/concept-extractor.bat
+++ Stanford/English/srl/concept-extractor.bat
@@ -1,3 +1,7 @@
+@echo off
+cd ..\stanford-parser
+java -mx150m -cp "*;" edu.stanford.nlp.parser.lexparser.LexicalizedParser -outputFormat "penn" edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz ..\srl\input.txt > ..\srl\parse-output.txt
+cd ..\srl
 python srl-identifier.py
 cd ..\identifier\maxent
 java -classpath .;./lib/trove-3.0.3.jar;./output/classes Predict train.test > output.txt
@@ -5,3 +9,4 @@ cd ..\..\classifier\maxent
 java -classpath .;./lib/trove-3.0.3.jar;./output/classes Predict train.test > output.txt
 cd ..\..\srl
 python srl-labeler.py > output.txt
+echo 'Done!'
