1a645886af1ddddd40dec92b98eea69bd0ad283c
shafqatvirk <virk.shafqat@gmail.com>
2014-05-17 12:20:25 +0800

    small change in concept formulator

--- Stanford/English/PyJava/concept-formulator.py
+++ Stanford/English/PyJava/concept-formulator.py
@@ -6,38 +6,43 @@ def build():
 		output = open('output.txt','a')
 		for (ident,conc) in zip(identifications,concepts):
 			if ident.split(' ')[0] == 'yes':
-							arg_str = conc.split(' ')[1]
-							pred_lemma = conc.split(' ')[0]
-							label = conc.split(' ')[2]
-							flat_arg_str = arg_str.replace(' ','_')
+						arg_str = conc.split(' ')[1]
+						pred_lemma = conc.split(' ')[0]
+						label = conc.split(' ')[2]
+						flat_arg_str_list = arg_str.split('and')
+						flat_arg_str_list.append(arg_str)
+						for flat_arg in flat_arg_str_list: 
+							flat_arg_str = flat_arg.rstrip(' ').lstrip(' ').replace(' ','_').rstrip('_').lstrip('_')
 							if label.rstrip() == 'ARG0':
 									arg_list2.append(flat_arg_str+'_'+pred_lemma)
+									#arg_list2.append(flat_arg_str)
 							elif label.rstrip() == 'ARGM-COM':
 									arg_list2.append(pred_lemma+'_{with}_'+flat_arg_str)
 									arg_list2.append(flat_arg_str)
 							elif label.rstrip() == 'ARGM-LOC':
 									arg_list2.append(pred_lemma+'_{in}_'+flat_arg_str)
-									arg_list2.append(flat_arg_str)
+									arg_list2.append('{location}_'+flat_arg_str)
 							elif label.rstrip() == 'ARGM-DIR':
-									arg_list2.append(pred_lemma+'_{in_the_direction}_'+flat_arg_str)
+									arg_list2.append(pred_lemma+'_{direction}_'+flat_arg_str)
+									arg_list2.append('{direction}_'+flat_arg_str)
 							elif label.rstrip() == 'ARGM-PRP':
 									arg_list2.append(pred_lemma+'_{in_order_to}_'+flat_arg_str)
 									arg_list2.append(flat_arg_str)
 							elif label.rstrip() == 'ARGM-CAU':
 									arg_list2.append(pred_lemma+'_{because}_'+flat_arg_str)
-									arg_list2.append(flat_arg_str)
+									arg_list2.append('{cause}_'+flat_arg_str)
 							elif label.rstrip() == 'ARGM-NEG':
 									arg_list2.append(pred_lemma+'_{negation}_'+flat_arg_str)
-									arg_list2.append(flat_arg_str)
+									arg_list2.append('{negation}_'+flat_arg_str)
 							elif label.rstrip() == 'ARGM-GOL':
 									arg_list2.append(pred_lemma+'_'+flat_arg_str)
-									arg_list2.append(flat_arg_str)
+									arg_list2.append('{goal}'+flat_arg_str)
 							elif label.rstrip() == 'ARGM-MNR':
 									arg_list2.append(pred_lemma+'_'+flat_arg_str)
-									arg_list2.append(flat_arg_str)
+									arg_list2.append('{manner}_'+flat_arg_str)
 							elif label.rstrip() == 'ARGM-TMP':
 									arg_list2.append(pred_lemma+'_{when}_'+flat_arg_str)
-									arg_list2.append(flat_arg_str)
+									arg_list2.append('{time}_'+flat_arg_str)
 							elif label.rstrip() == 'ARGM-EXT':
 									arg_list2.append(pred_lemma+'_{by}_'+flat_arg_str)
 									arg_list2.append(flat_arg_str)
